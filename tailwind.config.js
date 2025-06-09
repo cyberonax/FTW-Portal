8356a07ebd99f98dce95687a318da4fe4bc519a3
cyberonax <vocybersyn@gmail.com>
2025-06-09 19:50:32 +0000

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./public/index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        darkBg: "#1E1E2F",
+        panelBg: "#2A2A3F",
+        accent: "#4BFFA5",
+      },
+    },
+  },
+  plugins: [],
+};
