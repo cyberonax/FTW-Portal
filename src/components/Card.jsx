2b45181afc4befcb935848690db3fe678c6704a3
cyberonax <vocybersyn@gmail.com>
2025-06-09 21:09:33 +0000

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default function Card({ title, children }) {
   return (
-    <div className="bg-panelBg rounded-lg shadow p-4 mb-6">
-      <h2 className="text-xl mb-3">{title}</h2>
+    <div className="bg-panelBg rounded-lg shadow-panel p-4 mb-6">
+      <h2 className="text-xl text-textPrimary mb-3">{title}</h2>
       {children}
     </div>
   );
